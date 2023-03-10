d5c1d9db9bd3c487fec756ff605572bf61b3ca1a
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-10 18:42:48 +0100

    feat : Ajout Entities à la DataBase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PathsModule } from './paths/paths.module';
+import { User } from './users/entities/user.entity';
+import { Path } from './paths/entities/path.entity';
 
 /**
  * Permet la liaison entre la base de données, les Entities et les Controleurs.
@@ -20,9 +24,13 @@ import { AppService } from './app.service';
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: [
+        User,
+        Path
       ],
       synchronize: true,
     }),
+    UsersModule,
+    PathsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
