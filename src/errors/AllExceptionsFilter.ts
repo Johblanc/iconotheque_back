207e005e0b7f36d896a7525cb78909616be67c6d
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-10 17:18:56 +0100

    feat : Gestion et Formatage des erreurs

--- src/errors/AllExceptionsFilter.ts
+++ src/errors/AllExceptionsFilter.ts
@@ -1,18 +1,27 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-
+/**
+ * Permet de récupérer l'enssemble des Exceptions issues des divers controleurs.
+ * Cette fonctionnalité formate l'erreur avant de la renvoyer dans la console et dans le front.
+ * @Responce .statusCode  : Valeur du Code Status de la responce
+ * @Responce .timestamp   : Moment d'aparition de l'erreur
+ * @Responce .path        : Route utilisée pour aboutir à cette erreur
+ * @Responce .message     : Information sur l'erreur
+ */
 @Catch(HttpException)
-export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+export class HttpExceptionFilter implements ExceptionFilter 
+{
+  catch(exception: HttpException, host: ArgumentsHost) 
+  {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const exept : string | {statusCode?: number, message?:string,error?:string} = exception.getResponse();
+
     if (typeof exept === "object")
     {
-      
       console.log( `${request.method} | ${status} | ${request.url}\n${exept.error}`)
       response
         .status(status)
