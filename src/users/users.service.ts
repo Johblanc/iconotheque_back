0b53e1f663eb1d218605bd0fbbf9f1f5dfe1a818
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-11 12:54:58 +0100

    feat : Ajout UsersService.findOneByName

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,7 +6,8 @@ import { User } from './entities/user.entity';
 /**
  * Liaison avec la table users de la BDD
  * 
- * *@v1 **create** : Demande de création d'un utilisateur
+ * *@v1 **create**          : Demande de création d'un utilisateur
+ * *@v1 **findOneByName**   : Trouver un utilisateur par son nom
  * 
  * @version v1
  */
@@ -22,8 +23,22 @@ export class UsersService
    * 
    * @version v1
    */
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) : Promise<User>
+  {
     return await User.create({...createUserDto}).save();
   }
 
+
+  /**
+   * Trouver un utilisateur par son nom
+   * 
+   * @param name nom de l'utilisateur recherché
+   * @returns l'utilisateur, si il existe, sinon null
+   * 
+   * @version v1
+   */
+  async findOneByName(name : string) : Promise<User | null>
+  {
+    return await User.findOneBy({name})
+  }
 }
