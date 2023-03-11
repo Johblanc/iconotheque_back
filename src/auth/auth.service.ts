492f59962dc5004cda25c8a5641e53aac6cecb79
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-11 13:44:43 +0100

    feat : Ajout AuthService.validateUser

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/entities/user.entity';
+
+/**
+ * Vérification du mot de passe et création du token pour le login
+ * 
+ * @version v1
+ */
+@Injectable()
+export class AuthService {
+    constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService
+    ) {}
+
+  /**
+   * Vérication de mot de passe
+   * @param name le nom de l'utilisateur
+   * @param password le mot de passe de l'utilisateur
+   * @returns l'utilisateur sans mot de passe
+   * 
+   * @version v1
+   */
+  async validateUser(name: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByName(name);
+    const isOk = await bcrypt.compare( password, user ? user.password : "");
+
+    if (user && isOk) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+}
