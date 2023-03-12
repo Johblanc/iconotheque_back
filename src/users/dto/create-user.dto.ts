291b5f0dd7e9625d047957c2b2ebe9b17f5b0eb8
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-12 12:05:15 +0100

    Doc : Create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -8,15 +8,18 @@ import { IsString, IsEmail, IsNotEmpty } from "class-validator";
  */
 export class CreateUserDto {
 
+  /** Nom de l'utilisateur */
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name : string ;
 
+  /** Email de l'utilisateur */
   @ApiProperty()
   @IsEmail()
   mail : string ;
 
+  /** Mot de passe de l'utilisateur */
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
