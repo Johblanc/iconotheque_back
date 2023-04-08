5b32d2a3dbacbd1f98c1f334e4a38c1ba659b14d
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-08 08:50:14 +0200

    Feat : Route Aspect Create

---
+++ src/aspects/aspects.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { AspectsService } from './aspects.service';
+import { CreateAspectDto } from './dto/create-aspect.dto';
+import { UpdateAspectDto } from './dto/update-aspect.dto';
+
+/**
+ * Routage et contrôle des requete pour la table aspects
+ *
+ * @v2 **create**  : Demande de création d'un aspect
+ * @v2 **findAll** : Demande de récupération des aspects du user
+ * @v2 **findOne** : Demande de récupération d'un aspect
+ * @v2 **update**  : Demande de modification d'un path
+ * @v2 **remove**  : Demande de suppression d'un path
+ *
+ * @version v2
+ */
+@Controller('aspects')
+export class AspectsController {
+  constructor(private readonly aspectsService: AspectsService) {}
+
+  /**
+   * Demande de création d'un aspect
+   *
+   * @param createAspectDto paramètres de création d'un aspect
+   * @param user l'auteur
+   * @returns le nouvel aspect
+   *
+   * @version v2
+   */
+  @Post()
+  async create(@Body() createAspectDto: CreateAspectDto, @GetUser() user: User) {
+    return {
+      message: "Création d'un nouveau Path",
+      data: await this.aspectsService.create(createAspectDto, user)
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.aspectsService.findAll();
+  }
+
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAspectDto: UpdateAspectDto) {
+    return this.aspectsService.update(+id, updateAspectDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.aspectsService.remove(+id);
+  }
+}
