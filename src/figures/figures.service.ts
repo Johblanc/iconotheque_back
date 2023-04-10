f084154f151b832afe1ca02735d47999ddfb3687
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-10 23:48:41 +0200

    Feat : Service Figure Delete All

--- src/figures/figures.service.ts
+++ src/figures/figures.service.ts
@@ -6,111 +6,139 @@ import { Figure } from './entities/figure.entity';
 
 /**
  * Liaison avec la table figures de la BDD
- * 
- * @v2 **create** : Demande d'ajout d'une figure à une icône
- * @v2 **update** : Demande de modification d'une figure dans une icône
- * @v2 **remove** : Demande de suppression d'une figure dans une icône
- * 
+ *
+ * @v2 **create**    : Demande d'ajout d'une figure à une icône
+ * @v2 **update**    : Demande de modification d'une figure dans une icône
+ * @v2 **remove**    : Demande de suppression d'une figure dans une icône
+ * @v2 **removeAll** : Demande de suppression de toutes les figures d'une icône
+ *
  * @version v2
  */
 @Injectable()
 export class FiguresService {
-
   /**
    * Demande d'ajout d'une figure à une icône
-   * 
+   *
    * @param icon icône à laquelle on souhaite ajouter la figure
    * @param path Path de la figure
    * @param aspect Aspect de la figure
    * @param order Position de la figure dans l'icône
-   * 
+   *
    * @returns La nouvelle figure
-   * 
+   *
    * @version v2
    */
-  async create(icon : Icon, path: Path, aspect : Aspect, order : number) : Promise<Figure> {
-    
-    if (order !== icon.figures.length + 1){
-      await Promise.all([...icon.figures.map(
-        async figure => {
-          if (figure.order >= order){
-            figure.order += 1 
-            return await figure.save()
+  async create(
+    icon: Icon,
+    path: Path,
+    aspect: Aspect,
+    order: number,
+  ): Promise<Figure> {
+    if (order !== icon.figures.length + 1) {
+      await Promise.all([
+        ...icon.figures.map(async (figure) => {
+          if (figure.order >= order) {
+            figure.order += 1;
+            return await figure.save();
           }
-          return figure
-        }
-      )])
+          return figure;
+        }),
+      ]);
     }
-    return await Figure.create({icon, path, aspect, order}).save() ;
+    return await Figure.create({ icon, path, aspect, order }).save();
   }
 
   /**
    * Demande de modification d'une figure dans une icône
-   * 
+   *
    * @param icon icône dans laquelle on souhaite modifier la figure
    * @param order Position actuelle de la figure dans l'icône
    * @param path Nouveau path de la figure
    * @param aspect Nouvel aspect de la figure
    * @param newOrder Nouvelle Position de la figure dans l'icône
-   * 
+   *
    * @returns La figure modifiée
-   * 
+   *
    * @version v2
    */
-  async update(icon : Icon, order : number, path: Path | null, aspect : Aspect | null, newOrder? : number) : Promise<Figure | null> {
-    const figure = await Figure.findOneBy({icon : { id : icon.id }, order}) ;
+  async update(
+    icon: Icon,
+    order: number,
+    path: Path | null,
+    aspect: Aspect | null,
+    newOrder?: number,
+  ): Promise<Figure | null> {
+    const figure = await Figure.findOneBy({ icon: { id: icon.id }, order });
 
     if (figure !== null) {
-      if (path !== null) figure.path = path ;
-      if (aspect !== null) figure.aspect = aspect ;
-      if (newOrder && newOrder !== order){
-        const switchFigure = await Figure.findOneBy({icon : { id : icon.id }, order : newOrder}) ;
+      if (path !== null) figure.path = path;
+      if (aspect !== null) figure.aspect = aspect;
+      if (newOrder && newOrder !== order) {
+        const switchFigure = await Figure.findOneBy({
+          icon: { id: icon.id },
+          order: newOrder,
+        });
         if (switchFigure !== null) {
-          switchFigure.order = order
-          await Promise.all([await switchFigure.save()])
+          switchFigure.order = order;
+          await Promise.all([await switchFigure.save()]);
         }
-        figure.order = newOrder ;
+        figure.order = newOrder;
       }
-      await figure.save()
+      await figure.save();
     }
 
-    return figure ;
+    return figure;
   }
 
   /**
    * Demande de suppression d'une figure dans une icône
-   * 
+   *
    * @param icon icône dans laquelle on souhaite supprimer la figure
    * @param order Position actuelle de la figure dans l'icône
-   * 
+   *
    * @returns La figure supprimée
-   * 
+   *
    * @version v2
    */
-  async remove( icon : Icon, order : number ) {
-
-    
-    if (order !== icon.figures.length){
-      await Promise.all([...icon.figures.map(
-        async fig => {
-          if (fig.order > order){
-            fig.order -= 1 ;
-            return await fig.save() ;
+  async remove(icon: Icon, order: number): Promise<Figure | null> {
+    if (order !== icon.figures.length) {
+      await Promise.all([
+        ...icon.figures.map(async (fig) => {
+          if (fig.order > order) {
+            fig.order -= 1;
+            return await fig.save();
+          } else if ((fig.order = order)) {
+            fig.order = icon.figures.length;
+            return await fig.save();
           }
-          else if (fig.order = order) {
-            fig.order = icon.figures.length ;
-            return await fig.save() ;
-          };
-          return fig ;
-        }
-      )]) ;
+          return fig;
+        }),
+      ]);
     }
 
-    const figure = await Figure.findOneBy({icon : { id : icon.id }, order : icon.figures.length}) ;
+    const figure = await Figure.findOneBy({
+      icon: { id: icon.id },
+      order: icon.figures.length,
+    });
 
     if (figure !== null) {
-      await figure.remove()
+      await figure.remove();
     }
     return figure;
+  }
+
+  /**
+   * Demande de suppression de toutes les figures d'une icône
+   *
+   * @param icon icône dans laquelle on souhaite supprimer les figures
+   *
+   * @returns Les figure supprimées
+   *
+   * @version v2
+   */
+  async removeAll(icon: Icon): Promise<Figure[]>  {
+    await Promise.all([...icon.figures.map(async (fig) => await fig.remove())]);
+
+    return icon.figures;
   }
 }
